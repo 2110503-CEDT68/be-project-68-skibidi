65ea8bc820ac88546b2a812063d4e598d480bb36
Lyretter <japan326602007@gmail.com>
2026-02-28 14:25:11 +0700

    Fix route

--- routes/reservations.js
+++ routes/reservations.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const {getAppointments,getAppointment,addAppointment, updateAppointment,deleteAppointment}= require('../controllers/reservations');
+const {getReservations,getReservation,addReservation, updateReservation,deleteReservation}= require('../controllers/reservations');
 
 const router = express.Router({mergeParams:true});
 const {protect,authorize} =require('../middleware/auth');
 //router.route('/').get(getAppointments);
 
 router.route('/').get(protect,getReservations)
-.post(protect,authorize('admin','user'),addReservations);
+.post(protect,authorize('admin','user'),addReservation);
 
 router.route('/:id').get(protect,getReservation)
 .put(protect,authorize('admin','user'),updateReservation)
